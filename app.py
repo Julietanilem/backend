01d031a3ddcc721596486f64e554fb774d7a0cb8
Julieta <Julietanilem@gmail.com>
2024-10-05 20:21:51 -0600

    Primera versión de la app Flask

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+CORS(app)  # Permitir solicitudes CORS
+
+@app.route('/api/exoplaneta', methods=['POST'])
+def obtener_info_exoplaneta():
+    data = request.get_json()
+    nombre_exoplaneta = data.get('nombre')
+    
+    # Hacer el scraping (puedes cambiar la URL y los selectores según tus necesidades)
+    url = f'https://ejemplo.com/exoplanetas/{nombre_exoplaneta}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Extraer la información deseada
+    info = soup.find('div', class_='info-exoplaneta').text
+    
+    # Devolver la información al frontend
+    return jsonify({'nombre': nombre_exoplaneta, 'informacion': info})
+
+if __name__ == '__main__':
+    app.run(debug=True)
